d9802f05c7671f2f2c33f341059cabd5733ff03d
Jeremy Roy <jezoffy@gmail.com>
2023-10-17 16:10:36 +0100

    OMP matrix mult v1

--- OMPmatrixMult.cpp
+++ OMPmatrixMult.cpp
@@ -1,6 +1,58 @@
 #include <stdio.h>
 #include <omp.h>
+#include <iostream>
+#include <stdlib.h>
+
+#define size 10
+
+void multiplyMatrix(int A[size][size], int B[size][size], int C[size][size]) {
+    int i, j, k;
+
+    // Parallelize the outer loop; private variables: j, k; shared variables: A, B, C
+    #pragma omp parallel for private(j, k) shared (A, B, C)
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            C[i][j] = 0;
+            for (k = 0; k < size; k++){
+                // Compute the element of C at position (i, j) by summing products of elements from A and B
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void printMatrix(char *name, int M[size][size]) {
+    printf("Matrix %c:\n", name);
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    
+    int A[size][size];
+    int B[size][size];
+    int C[size][size];
+
+    // Initialize matrices A and B with random values
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            A[i][j] = rand() % 10; // Random value between 0 and 9
+            B[i][j] = rand() % 10;
+        }
+    }
+
+    // Perform matrix multiplication using OpenMP
+    multiplyMatrix(A, B, C);
+
+    // Print matrix A
+    printMatrix("A", A);
+
+    // Print matrix B
+    printMatrix("B", B);
+
+    // Print matrix C
+    printMatrix("Result C", C);
 }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HPC-Programming
+
 A place where I work on general HPC Projects and run them on a GPU of some sort.
+
+1) Matrix Multiplication
+
+Matrix multiplication is a problem suitable for parallelization using both OpenMP (OMP) and MPI (Message Passing Interface) due to its inherent parallel nature. When multiplying two matrices, each element of the resulting matrix is independent of the others, making it a good candidate for parallel computation. OpenMP is suitable for shared-memory parallelism on multi-core systems, while MPI is suitable for distributed-memory parallelism, often used on clusters or supercomputers.
